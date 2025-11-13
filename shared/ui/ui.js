476cb0e1055b75a9f3144d3aec798771e1790b68
SaFr0niY <dinnor12345@gmail.com>
2025-11-13 12:51:16 +0500

    CreateJs2

--- shared/ui/ui.js
+++ shared/ui/ui.js
@@ -1,7 +1,25 @@
+import Image from "next/image"
 
+export const Cover = ({src}) =>{
+    return(
+        <image src={src} alt="обложка"/>
+    )
+}
 
 export const Title = ({children}) =>{
     return(
         <span>{Children}</span>
+    )
+}
+
+export const Artist = ({nickName}) =>{
+    return(
+        <p>{nickName}</p>
+    )
+}
+
+export const Duration = ({duration}) =>{
+    return(
+        <span>{duration}</span>
     )
 }
